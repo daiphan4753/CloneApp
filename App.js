a03c079925cc330b050d935e921ebc3d6c3aef4c
daiphan4753 <daiphan4753@gmail.com>
2021-09-17 23:15:25 +0700

    init app

---
+++ App.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import {Text, StyleSheet, View} from 'react-native';
+
+export default App = () => {
+  return (
+    <View>
+      <Text>this is App</Text>
+    </View>
+  )
+}
